e3c041c9a50613b1c2331eac5bf83d83aef9102f
alrav <rav2315@gmail.com>
2019-12-07 14:55:18 -0800

    Initial bootstrap

---
+++ src/extension.ts
@@ -0,0 +1,10 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand('extension.3WayMerge', () => {
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
