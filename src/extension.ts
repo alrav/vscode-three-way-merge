80919018265183cf0aa9a230a3bae490366b80ad
alrav <rav2315@gmail.com>
2019-12-14 08:16:45 -0800

    Create dummy extension with mock WebView

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,33 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('extension.3WayMerge', () => {
-	});
+	console.log('ThreeWayMerge extension is activated');
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('extension.threeWayMerge', () => {
+			const panel = vscode.window.createWebviewPanel(
+				'threeWayMerge',
+				'foo.js - merge',
+				vscode.ViewColumn.One,
+			);
+
+			panel.webview.html = getWebviewContent();
+		})
+	);
+}
+
+function getWebviewContent() {
+return `<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>foo.js - merge</title>
+</head>
+<body>
+	<p>foo</p>
+</body>
+</html>`;
 }
 
 export function deactivate() {}
