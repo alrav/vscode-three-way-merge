0f8de40ac5e008c5afb68d12c37b6ae1684c33b3
alrav <rav2315@gmail.com>
2019-12-22 13:31:28 -0800

    Load current open text editor document into webview

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,50 @@
 import * as vscode from 'vscode';
 
+function getTextDocumentLines(textDocument: vscode.TextDocument): Array<string> {
+	const lines = [];
+
+	for (let i = 0; i < textDocument.lineCount; i++) {
+		lines.push(textDocument.lineAt(i).text);
+	}
+
+	return lines;
+}
+
+const textDocumentLinesToHtml = (lines: Array<string>) =>
+	lines.reduce((html: string, line: string) => html += `<p>${line}</p>`);
+
 export function activate(context: vscode.ExtensionContext) {
 	console.log('ThreeWayMerge extension is activated');
 
 	context.subscriptions.push(
-		vscode.commands.registerCommand('extension.threeWayMerge', () => {
+		vscode.commands.registerTextEditorCommand('extension.threeWayMerge', (textEditor: vscode.TextEditor) => {
+			const fileName = textEditor.document.fileName;
+			const baseFileName = fileName.split('/').pop() || '';
+
 			const panel = vscode.window.createWebviewPanel(
 				'threeWayMerge',
-				'foo.js - merge',
+				`${baseFileName} - merge`,
 				vscode.ViewColumn.One,
 			);
 
-			panel.webview.html = getWebviewContent();
+			panel.webview.html = getWebviewContent(
+				baseFileName,
+				textDocumentLinesToHtml(getTextDocumentLines(textEditor.document)),
+				);
 		})
 	);
 }
 
-function getWebviewContent() {
+function getWebviewContent(baseFileName: string, textDocumentLinesAsHtml: string) {
 return `<!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>foo.js - merge</title>
+	<title>${baseFileName} - merge</title>
 </head>
 <body>
-	<p>foo</p>
+	${textDocumentLinesAsHtml}
 </body>
 </html>`;
 }
